7b68f92d7684bf1408190f32b75b1c6585f57f5e
pashay94 <pascom_by@mail.ru>
2024-12-29 06:52:41 +0300

    feat: solve names,emal,mac,matrix,sweeper tasks

--- src/file-names.js
+++ src/file-names.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * There's a list of file, since two files cannot have equal names,
@@ -15,11 +15,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  const obj = {};
+  const res = [];
+  names.forEach((el) => {
+    if (!obj[el]) {
+      obj[el] = 1;
+      res.push(el);
+    } else {
+      res.push(`${el}(${obj[el]})`);
+      obj[`${el}(${obj[el]})`] = 1;
+      obj[el] += 1;
+    }
+  });
+  return res;
 }
 
 module.exports = {
-  renameFiles
+  renameFiles,
 };
