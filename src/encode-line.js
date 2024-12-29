4afe94a4d6393e758166913b415de95273ae4858
pashay94 <pascom_by@mail.ru>
2024-12-29 04:58:00 +0300

    feat: solve dream,encode,repeater tasks

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a string, return its encoding version.
@@ -10,11 +10,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let count = 0;
+  let res = "";
+  str.split("").reduce((ac, el, ind, arr) => {
+    if (ac === el) {
+      count += 1;
+    } else {
+      res += count > 1 ? `${count}${ac}` : `${ac}`;
+      count = 1;
+    }
+    if (arr.length - 1 === ind) res += count > 1 ? `${count}${el}` : `${el}`;
+    return el;
+  }, "");
+  return res;
 }
 
 module.exports = {
-  encodeLine
+  encodeLine,
 };
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,32 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const arr = [];
+  arr.length = options.repeatTimes || 1;
+  const add = [];
+  add.length = options.additionRepeatTimes || 1;
+  add.fill(
+    String(options.addition) === "undefined" ? "" : String(options.addition)
+  );
+  arr.fill(str + add.join(options.additionSeparator || "|"));
+  return arr.join(options.separator || "+");
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
